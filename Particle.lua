d3b8770e1c293325214abb1d279aef0c01eb09a1
numberarc <org.number.arc@gmail.com>
2026-07-05 10:05:11 -0500

    Create Particle.lua

---
+++ Particle.lua
@@ -0,0 +1,8 @@
+-- Innovation labs will let you fire the TP remote and make you become invisible but with PARTICLES. 
+-- So what i did is make it spam it as shown below.
+-- Script by Team Sersku
+
+while true do
+    game:GetService('ReplicatedStorage'):WaitForChild('TP'):FireServer()
+    wait(0.1)     
+end
